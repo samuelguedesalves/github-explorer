95b4f002c055da8d0e12c312e2bacd38fb4ac82f
Samuel Guedes <samuel.moura@credisis.com.br>
2022-01-08 20:56:11 -0400

    builds: config project to use typescript

--- src/index.jsx
+++
@@ -1,5 +0,0 @@
-import './styles/global.scss';
-import { render } from "react-dom";
-import App from './App';
-
-render(<App/>, document.getElementById('root'));
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import './styles/global.scss'
+import { render } from 'react-dom'
+import App from './App'
+
+render(<App />, document.getElementById('root'))
